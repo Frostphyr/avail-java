4d0036aff0a516b568f09274dd61cd5ef7e1cb5d
Frostphyr <omoshulol@gmail.com>
2021-02-12 18:30:50 -0500

    Added package-info.java for each package

---
+++ src/main/java/com/frostphyr/avail/math/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains utility classes that assist with performing 
+ * mathematical operations.
+ * 
+ * @since 1.0
+ */
+package com.frostphyr.avail.math;
---
+++ src/main/java/com/frostphyr/avail/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains miscellaneous utility classes that don't fit into 
+ * a more specific subpackage.
+ * 
+ * @since 1.0
+ */
+package com.frostphyr.avail;
