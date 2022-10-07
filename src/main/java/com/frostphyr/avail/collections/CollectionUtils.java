9743c93ab5e579424be5e35ad0af2533bc205bb6
Frostphyr <omoshulol@gmail.com>
2022-10-07 13:37:36 -0400

    Updated documentation

--- src/main/java/com/frostphyr/avail/collections/CollectionUtils.java
+++ src/main/java/com/frostphyr/avail/collections/CollectionUtils.java
@@ -17,49 +17,44 @@ public final class CollectionUtils {
 	}
 	
 	/**
-	 * Returns an immutable view of the specified {@link Iterator}. The 
-	 * method that can modify the collection, {@link Iterator#remove()}, 
-	 * will throw an {@link UnsupportedOperationException}. Otherwise, it will 
-	 * defer to the specified {@link Iterator}.
+	 * Returns an immutable view of {@code iterator}. It will throw 
+	 * {@link UnsupportedOperationException} if {@link Iterator#remove()} is 
+	 * called. Otherwise, it will defer any call to the {@code iterator}.
 	 * 
-	 * @param <T> the type of the {@link Iterator}'s generic parameter
+	 * @param <T> the parameter type of the specified and returned 
+	 * {@link Iterator}
 	 * @param iterator the {@link Iterator} to create an immutable view for
-	 * @return an immutable view of the specified {@link Iterator}
+	 * @return an immutable view of {@code iterator}
+	 * @throws IllegalArgumentException if {@code iterator} is {@code null}
 	 */
-	public static <T> Iterator<T> immutableIterator(final Iterator<T> iterator) {
-		return new ImmutableIterator<T>(iterator);
-	}
-	
-	private static class ImmutableIterator<T> implements Iterator<T> {
+	public static <T> Iterator<T> immutableIterator(Iterator<T> iterator) {
+		Argument.checkNotNull(iterator, "iterator");
 		
-		private Iterator<T> iterator;
-		
-		private ImmutableIterator(Iterator<T> iterator) {
-			this.iterator = Argument.checkNotNull(iterator);
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return iterator.hasNext();
-		}
+		return new Iterator<T>() {
+			
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
 
-		@Override
-		public T next() {
-			return iterator.next();
-		}
+			@Override
+			public T next() {
+				return iterator.next();
+			}
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-		
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+			
+		};
 	}
 	
 	/**
-	 * Returns an immutable view of the specified {@link ListIterator}. The 
-	 * methods that can modify the collection will throw an 
-	 * {@link UnsupportedOperationException}. Otherwise, it will 
-	 * defer to the specified {@link ListIterator}.
+	 * Returns an immutable view of {@code iterator}. The methods that can 
+	 * modify the collection will throw an 
+	 * {@link UnsupportedOperationException}. Otherwise, it will defer any 
+	 * call to the {@code iterator}.
 	 * <p>
 	 * The methods that can modify the collection and will throw an 
 	 * {@link UnsupportedOperationException} are:
@@ -69,67 +64,63 @@ public final class CollectionUtils {
 	 *     <li>{@link ListIterator#add(Object)}</li>
 	 * </ul>
 	 * 
-	 * @param <T> the type of the {@link ListIterator}'s generic parameter
+	 * @param <T> the parameter type of the specified and returned 
+	 * {@link ListIterator}
 	 * @param iterator the {@link ListIterator} to create an immutable view for
-	 * @return an immutable view of the specified {@link ListIterator}
+	 * @return an immutable view of {@code iterator}
+	 * @throws IllegalArgumentException if {@code iterator} is {@code null}
 	 */
 	public static <T> ListIterator<T> immutableListIterator(final ListIterator<T> iterator) {
-		return new ImmutableListIterator<T>(iterator);
-	}
-	
-	private static class ImmutableListIterator<T> implements ListIterator<T> {
+		Argument.checkNotNull(iterator, "iterator");
 		
-		private ListIterator<T> iterator;
-		
-		private ImmutableListIterator(ListIterator<T> iterator) {
-			this.iterator = Argument.checkNotNull(iterator);
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return iterator.hasNext();
-		}
+		return new ListIterator<T>() {
+			
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
 
-		@Override
-		public T next() {
-			return iterator.next();
-		}
+			@Override
+			public T next() {
+				return iterator.next();
+			}
 
-		@Override
-		public boolean hasPrevious() {
-			return iterator.hasPrevious();
-		}
+			@Override
+			public boolean hasPrevious() {
+				return iterator.hasPrevious();
+			}
 
-		@Override
-		public T previous() {
-			return iterator.previous();
-		}
+			@Override
+			public T previous() {
+				return iterator.previous();
+			}
 
-		@Override
-		public int nextIndex() {
-			return iterator.nextIndex();
-		}
+			@Override
+			public int nextIndex() {
+				return iterator.nextIndex();
+			}
 
-		@Override
-		public int previousIndex() {
-			return iterator.previousIndex();
-		}
+			@Override
+			public int previousIndex() {
+				return iterator.previousIndex();
+			}
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
 
-		@Override
-		public void set(T e) {
-			throw new UnsupportedOperationException();
-		}
+			@Override
+			public void set(T e) {
+				throw new UnsupportedOperationException();
+			}
 
-		@Override
-		public void add(T e) {
-			throw new UnsupportedOperationException();
-		}
-		
+			@Override
+			public void add(T e) {
+				throw new UnsupportedOperationException();
+			}
+			
+		};
 	}
 
 }
